9a75f3b639a143dbfed350bba1d4eadc5a931bdd
mlmms <47452600+mlmms@users.noreply.github.com>
2020-04-15 17:19:42 +0100

    Update N4algorithm_it_local.py

--- 4_Preprocessing/T2/N4algorithm_it_local.py
+++ 4_Preprocessing/T2/N4algorithm_it_local.py
@@ -9,14 +9,13 @@ list = os.listdir(path)
 list.sort()
 
 for file in list:
-    if file.endswith("033_T2ax.nii"):
-        print(file)
-        img = sitk.ReadImage(os.path.join(path, file))
+    print(file)
+    img = sitk.ReadImage(os.path.join(path, file))
 
-        image = sitk.Cast(img, sitk.sitkFloat32)  # Required to have a “real” pixel type: sitkFloat32 or sitkFloat64.
-        corrected_img = sitk.N4BiasFieldCorrection(image)
+    image = sitk.Cast(img, sitk.sitkFloat32)  # Required to have a “real” pixel type: sitkFloat32 or sitkFloat64.
+    corrected_img = sitk.N4BiasFieldCorrection(image)
 
-        sitk.WriteImage(corrected_img, os.path.join(output, 'NB_' + file))
-        print("Finished N4 Bias Field Correction of ", file)
+    sitk.WriteImage(corrected_img, os.path.join(output, 'NB_' + file))
+    print("Finished N4 Bias Field Correction of ", file)
 
 print("Finished N4 for all .nii files")
