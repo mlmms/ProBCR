3521f2b5ef99c6c78579a7ef7b1f425f6a8af8a9
mlmms <47452600+mlmms@users.noreply.github.com>
2020-12-16 18:09:33 +0000

    Add files via upload

---
+++ 7_Postprocessing_ImageCropping_BoundingBox_Normalization/A.0_crop_image_T2ax_I.py
@@ -0,0 +1,96 @@
+# Aim: Crop image for normalization purposes
+# Status: properly working
+# MMS 2019
+
+#INPUT:
+# 1) original image
+# 2) original label
+#OUTPUT:
+# 1) resampled label (reference: image)
+# 2) cropped image (result of ' Image * Resampled Label ' )
+
+# The resample of the Label is more important in the case of DWI images and ADC, where the Labels were obtained after
+# the Registration process. In the T2ax case, it would not be necessary, because the labels were created upon the original T2 images
+
+import glob, os
+import numpy as np
+import SimpleITK as sitk
+from shutil import copyfile
+#from numpy import * #all functions will be loaded into the local namespace
+
+multiply=sitk.MultiplyImageFilter()
+
+src = "/Users/monicasilva/Documents/1_Original_data/Final_Cohort_2020_99p/preRadiomics/T2*Mask_i"
+os.chdir(src)
+
+images = glob.glob("*ax.nii")
+images.sort()
+print("images = ", images)
+
+dst = "/Users/monicasilva/Documents/1_Original_data/Final_Cohort_2020_99p/preRadiomics/T2*Mask_i/output"
+
+masks = glob.glob("*label.nii")
+masks.sort()
+print("masks = ", masks)
+
+### Reading images to numpy arrays ###
+for (i, j) in zip(images, masks):
+
+    a = i
+    f = j
+    i = os.path.join(src, i)
+    j = os.path.join(src, j)
+    img = sitk.ReadImage(i)
+    mask = sitk.ReadImage(j)
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(img)
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+    mask = resampler.Execute(mask)
+    filename_resampled_mask = "res_" + f
+
+    #OUTPUT 1)
+    sitk.WriteImage(mask, os.path.join(dst, filename_resampled_mask))  # save final, and resampled, (DWI) label
+    print("Finished resampling of ", a)
+
+
+    print(i)
+    print(img.GetPixelIDTypeAsString())
+    print("Image size: " + str(img.GetSize()))
+    print("Original Spacing: ", img.GetSpacing())
+    print("Original Origin: ", img.GetOrigin())
+    print("Direction: ", img.GetDirection())
+
+    print(j)
+    print(mask.GetPixelIDTypeAsString())
+    print("Image size: " + str(mask.GetSize()))
+    print("Original Spacing: ", mask.GetSpacing())
+    print("Original Origin: ", mask.GetOrigin())
+    print("Direction: ", mask.GetDirection())
+
+
+    img = sitk.Cast(img, sitk.sitkFloat64)
+
+    mask = sitk.Cast(mask, sitk.sitkFloat64)
+
+    #OUTPUT 2):
+    crp_img = multiply.Execute(img, mask)
+
+    print(crp_img.GetPixelIDTypeAsString())
+    print("Image size: " + str(crp_img.GetSize()))
+    print("Original Spacing: ", crp_img.GetSpacing())
+    print("Original Origin: ", crp_img.GetOrigin())
+    print("Direction: ", crp_img.GetDirection())
+
+
+    sitk.WriteImage(crp_img, os.path.join(dst, 'crp' + a))
+    #copyfile(os.path.join(src,f), os.path.join(dst,f))
+    print("Finished cropping of ", a)
+
+
+
+
+# Crop a bit outside the segmentation (rectangular)
+# 1. Calculate mask dimensions (a x b)
+# 2. Create rectangle (a x b)
+# 3. Multiply image with rectangle
---
+++ 7_Postprocessing_ImageCropping_BoundingBox_Normalization/B_crop_images_pyradiomics_b1000ii.py
@@ -0,0 +1,64 @@
+# DWI, b1000 radiomics, for multiparametric MRI, for PCa * MMS, 2019
+# Analysis II
+
+import os
+import radiomics
+from radiomics import featureextractor
+from radiomics import imageoperations
+import SimpleITK as sitk
+import glob
+
+# 0_Setting up data
+
+#2nd analysis:  PROSTATE and surrounding tissues
+src = "/Users/monicasilva/Documents/1_Original_data/Final_Cohort_2020_99p/pre_PyRadiomics/6.2_DWIb1000/Analysis_II(withbackground)/3_BB_input"
+dst = "/Users/monicasilva/Documents/1_Original_data/Final_Cohort_2020_99p/pre_PyRadiomics/6.2_DWIb1000/Analysis_II(withbackground)/4_BB_output"
+
+os.chdir(src)
+
+list_images = sorted(glob.glob("*b1000_f.nii"))
+cases = list_images
+for i in range(len(list_images)):
+    cases[i] = list_images[i][0:4]
+
+#list_images = sorted(glob.glob("ADC*"))
+
+list_images = sorted(glob.glob("*b1000_f.nii"))
+list_labels = sorted(glob.glob("*label.nii"))
+
+for (i, j) in zip(list_images, list_labels):
+    case = i[0:4]
+
+    case1 = case + str("_crpBB.nii")
+    case2 = case + str("_crpBB_mask.nii")
+    imagePath = os.path.join(src, i)
+    labelPath = os.path.join(src, j)
+    #paramPath = "/Users/monicasilva/PycharmProjects/ProBCR/radiomics_kit/data.yml"
+
+    image = sitk.ReadImage(imagePath)
+    label = sitk.ReadImage(labelPath)
+    label = sitk.Cast(label, sitk.sitkUInt8)
+    #radiomics.setVerbosity(level=10)
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(image)
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+    label2 = resampler.Execute(label)
+
+    out = radiomics.imageoperations.checkMask(image, label2)
+    boundingBox_mask = out[0]
+
+    out2 = radiomics.imageoperations.cropToTumorMask(image, label2, boundingBox_mask)
+    crp_image = out2[0]
+    crp_mask = out2[1]
+    #OUTPUTS:
+    sitk.WriteImage(crp_image, os.path.join(dst, case1))
+    sitk.WriteImage(crp_mask, os.path.join(dst, case2))
+
+    #ADC map: no normalization!!!!
+    norm_crp_img = radiomics.imageoperations.normalizeImage(crp_image)
+    case3 = case + str("_crpBB_norm.nii")
+    #OUTPUT:
+    sitk.WriteImage(norm_crp_img, os.path.join(dst, case3))
+
+    print("Finished case ", case)
